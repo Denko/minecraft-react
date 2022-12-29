3630db235dc67e4b7d6ff1485e46571ce59a71c2
Alejandro Arco <88550707+Denko@users.noreply.github.com>
2022-12-29 19:00:49 +0100

    inicializado

---
+++ src/images/textures.js
@@ -0,0 +1,10 @@
+import { grassImg } from "./images";
+
+import { RepeatWrapping, TextureLoader } from "three";
+
+const groundTexture = new TextureLoader().load(grassImg)
+
+groundTexture.wrapS = RepeatWrapping
+groundTexture.wrapT = RepeatWrapping
+
+export { groundTexture }
